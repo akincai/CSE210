8eb2b162da6ff845b842200e86cd272618082a54
akincai <u763579476@byui.edu>
2024-06-15 14:13:03 -0700

    finished Prep3 requirements

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int number = new Random().Next(1, 100);
+
+        // initialize guess as -1 as it is outside the range of random numbers and cannot match the number before the loop runs
+        int guess = -1;
+        while(guess != number)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (number > guess)
+                Console.WriteLine("Higher");
+            else if (number < guess)
+                Console.WriteLine("Lower");
+            else
+                Console.WriteLine("You guessed it!");
+        }
     }
 }
