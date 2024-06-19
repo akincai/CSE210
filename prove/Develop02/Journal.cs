37356e9a6d41c6027fd816f5466a56ab0e6a2f41
akincai <u763579476@byui.edu>
2024-06-19 12:04:43 -0700

    added missing parameters to Journal.cs

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -2,7 +2,7 @@ public class Journal
 {
     public List<Entry> _entries = new List<Entry>();
 
-    public void AddEntry()
+    public void AddEntry(Entry newEntry)
     {
 
     }
@@ -12,12 +12,12 @@ public class Journal
 
     }
 
-    public void SaveToFile()
+    public void SaveToFile(string file)
     {
 
     }
 
-    public void LoadFromFile()
+    public void LoadFromFile(string file)
     {
 
     }
