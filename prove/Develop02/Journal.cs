ba65b9d6829952c2aa7c24e6d472521b6a24a3d5
akincai <u763579476@byui.edu>
2024-06-19 14:00:10 -0700

    added functionality for class methods

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -4,21 +4,42 @@ public class Journal
 
     public void AddEntry(Entry newEntry)
     {
-
+        _entries.Add(newEntry);
     }
     
     public void DisplayAll()
     {
-
+        foreach (Entry e in _entries)
+        {
+            e.Display();
+        }
     }
 
     public void SaveToFile(string file)
     {
-
+        using (StreamWriter outputFile = new StreamWriter(file))
+        {
+            foreach (Entry e in _entries)
+            {
+                outputFile.WriteLine($"{e._promptText}*{e._entryText}*{e._date}");
+            }
+        }
     }
 
     public void LoadFromFile(string file)
     {
+        string[] lines = System.IO.File.ReadAllLines(file);
+        foreach (string line in lines)
+        {
+            // asterisks for partitioning since commas are commonly used in writing
+            string[] parts = line.Split("*");
 
+            Entry newEntry = new Entry();
+            newEntry._promptText = parts[0];
+            newEntry._entryText = parts[1];
+            newEntry._date = parts[2];
+
+            this.AddEntry(newEntry);
+        }
     }
 }
--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -2,8 +2,17 @@ public class PromptGenerator
 {
     public List<string> _prompts = new List<string>();
 
+    public PromptGenerator()
+    {
+        _prompts.Add("Who was the most interesting person I interacted with today?");
+        _prompts.Add("What was the best part of my day?");
+        _prompts.Add("If I had one thing I could do over today, what would it be?");
+        _prompts.Add("What did I improve about myself today?");
+        _prompts.Add("What can I do better tomorrow?");
+    }
+
     public string GetRandomPrompt()
     {
-        return "";
+        return _prompts[new Random().Next(_prompts.Count)];
     }
 }
