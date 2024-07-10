f4ce74f2611ca126c1e1a885424fa16aed045aa3
akincai <u763579476@byui.edu>
2024-07-09 23:48:23 -0700

    finished Learning05 requirements

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square(10, "green"));
+        shapes.Add(new Square(2.2, "blue"));
+        shapes.Add(new Rectangle(100, 2, "orange"));
+        shapes.Add(new Rectangle(10, 4, "red"));
+        shapes.Add(new Circle(42.3, "purple"));
+        shapes.Add(new Circle(2.5, "black"));
+
+        foreach (Shape s in shapes)
+            Console.WriteLine($"The {s.GetColor()} {s.GetType().ToString().ToLower()} has an area of {s.GetArea()}");
     }
 }
---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,17 @@
+public abstract class Shape
+{
+    private string _color;
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+
+    public abstract double GetArea();
+
+    public string GetColor()
+    {
+        return _color;
+    }
+}
